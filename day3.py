e7cd981259cd02f559f062a6cc2579af4fa66fa3
mohammadzeidan <mohammadz20012001@Gmail.com>
2025-09-24 19:24:41 +0300

    pic

--- day3.py
+++ day3.py
@@ -10,42 +10,43 @@
 #-works offline
 #cd /your path
 #track changes in your project
-# git init
-#-initialize a new git repository
-# -creates a .git directory
-#-start tracking changes
-#git status
-#-check the status of your repository
-print("installed git") 
-#2 ways 2 use git bash
+
+ git init
+# -initialize a new git repository
+#  -creates a .git directory
+# -start tracking changes
+git status
+# -check the status of your repository
+# print("installed git") 
+# 2 ways 2 use git bash
 # 1. git bash terminal
 # 2. vs code terminal
-#git config --global user.name "your name"
-#git config --global user.email "your email"
-#git config --list
-#after opening the bash on the correct path
-# git init initialize the git repository(follow all changes in the project)
-#once changes are done on a fle or more 
-#git add <file name> will let git track changes on that file
-#after adding the changed files
-#git commit -m "your message"
-#-commit the changes with a message
-# git add .
-#git reset HEAD
+git config --global user.name "your name"
+git config --global user.email "your email"
+git config --list
+# after opening the bash on the correct path
+|git init| initialize the git repository(follow all changes in the project)
+# once changes are done on a fle or more 
+git add <file name> will let git track changes on that file
+# after adding the changed files
+git commit -m "your message"
+# -commit the changes with a message
+git add .
+git reset HEAD
 
-#if u git add . by mistake
-#git reset HEAD <file name>
-#-unstage a file
-#git log
-#git commit -m "your message"
-#-view commit history
-#remove commit from history
-#git reset --soft HEAD~1
+if u git add . by mistake
+git reset HEAD <file name>
+-unstage a file
+git log
+git commit -m "your message"
+-view commit history
+remove commit from history
+git reset --soft HEAD~1
 
 print("git and github")
-#-remove the last commit but keep changes in working directory
-#git reset --hard HEAD~1
-#-remove the last commit and discard changes
-#git checkout -- <file name>
-#-discard changes in a file
+-remove the last commit but keep changes in working directory
+git reset --hard HEAD~1
+-remove the last commit and discard changes
+git checkout -- <file name>
+-discard changes in a file
 print("github")
