a4b3e0b0093fde4b248554421387250bbd81e924
mohammadzeidan <mohammadz20012001@Gmail.com>
2025-09-26 18:32:06 +0300

    new branch

--- branching.py
+++ branching.py
@@ -1,1 +1,9 @@
 #branching
+#HOW TO LIST ALL THE BRANCHES
+git branch
+#HOW TO CREATE A NEW BRANCH
+git  CHECKOUT -B  "branch_name" :creates a new branch
+git commit -m "added branch for master"
+#HOW TO SWITCH TO A BRANCH
+git checkout master
+git checkout branch_name
