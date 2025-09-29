9b41ad453a11593f79396998eaf38d09bcd2473d
mohammadzeidan <mohammadz20012001@Gmail.com>
2025-09-29 18:20:41 +0300

    github

--- branching.py
+++ branching.py
@@ -7,3 +7,14 @@ git commit -m "added branch for master"
 #HOW TO SWITCH TO A BRANCH
 git checkout master
 git checkout branch_name
+
+#ig i need to merge i must be in the master branch
+git merge branch_name\
+#vim comands
+#to save and exit :wq
+#to exit without saving :q!
+#to exit without saving forcefully :q!
+#to save without exiting :w
+#when fo vim opens
+#press i to enter insert mode
+#press esc to exit insert mode
---
+++ github.py
@@ -0,0 +1,5 @@
+first add reprository in github manually
+#then copy the link of the repository
+git remote add origin <repository link>
+#then push the code to the repository
+git push -u origin master
