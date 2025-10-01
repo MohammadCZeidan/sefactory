1910d8cf3009b713e215d672f66c73402845aab6
mohammadzeidan <mohammadz20012001@Gmail.com>
2025-10-01 19:22:51 +0300

    range  and loops and while

--- day6.py
+++ day6.py
@@ -1,2 +1,21 @@
 #last day was list 
 #today we will learn about tuple
+#data structures
+#list\
+my_list=[1,2,3,4,5]
+#tuple
+#what is the difference between list and tuple
+#list are mutable : we can change them
+#tuple are immutable : we can not change them
+#tuple is python specific data structure
+my_tuple=(1,2,3,4,5)
+len_tuple=len(my_tuple)
+print("my_tuple",my_tuple)
+my_tuple.append(6) #does not work
+my_tuple.extend([7,8,9]) #does not work
+my_tuple.insert(0,0) #does not work
+my_tuple.pop(3) #does not work
+my_ruple[2]="moe" #does not work
+#but we can access the elements of the tuple
+print(my_tuple[2]) #works
+
---
+++ loop6.py
@@ -0,0 +1,45 @@
+
+#flow control
+#loops
+# ny_lisrt = ["manhattan", "bronx", "brooklyn", "queens", "staten island"]    
+# for i in ny_lisrt:
+    # print(i)
+    #for is a keyword
+    #i is a variable
+    #in is a keyword
+    #ny_list is a list variable
+    # for bla in ny_lisrt:
+        # print(bla)
+        # print("1")
+        # print("hello world")
+        # x=10*5
+        # print(x)
+# for i in range(100):
+
+    # print(i)
+    #range:
+    #range(n) -> 0 to n-1
+     #range(start)
+    #range(m,n) -> m to n-1 
+    #range(start, stop)
+    #range(m,n,s) -> m to n-1 with step s sarts from m and add s each time
+#range(start, stop, step)
+# my_lisrt = ["manhattan", "bronx", "brooklyn", "queens", "staten island"]    
+# for index in range(0,len(my_lisrt)):
+    # print("index: ",index)
+    # print("value: ", my_lisrt[index])
+
+# for var_1 , var_2 in enumerate(my_lisrt):
+    # print("index: ",var_1)
+    # print("value: ", var_2)
+#enumerate
+#enumerate(list) -> (index, value)
+#while loop
+# while condition:
+# while True:
+    # print("hello world")#condition  we
+    # break
+conter =0
+while conter < 10:
+    print("Counter: ",conter)
+    conter = conter + 1
