99e676f98f70c42e02d9b8f27c040115e97826b6
mohammadzeidan <mohammadz20012001@Gmail.com>
2025-10-10 19:24:28 +0300

    day8 opp

---
+++ day9.py
@@ -0,0 +1,54 @@
+#oop
+#object oriented programing
+#list is a complex data type=> we call it data strcuture 
+
+
+#car
+#colior:blue
+#brand :toyota
+#engine:hybrid
+#windows:Tinted
+
+
+# name ="joe"
+# grade ="10"
+# names =["joe","moe"]
+# grades =["10",20]
+
+
+
+#create a class to group multipe attribute for a single variable
+####class =classname
+### attributes and method
+#the self key word represent the instance create then (not the class)defined the class
+class Student:#class StudentGrade
+    def __init__(self,name,grade):#self here is important
+        # print("om init")
+     self.name=""
+     self.grade=0
+
+    def intro_yourself(self):
+      print("hello, im ", self.name, "i got ", self.grade)
+     
+    def checked_if_passed(self):
+       if self.grade>=50:
+        print(self.name,"has passed")
+       else:
+           print(self.name,"has not passed")
+  
+#create an instance of the class
+s1= Student("joe",10)
+s2= Student("moe",20)
+
+print("s1")
+print(s1.name)
+print(s1.grade)
+print(type([1,2]))
+print(type([s1]))
+s1= Student("joe",10)
+s2= Student("moe",20)
+s1.intro_yourself()
+s2.intro_yourself()
+
+# for s in Student:
+#  s.checked_if_passed()
