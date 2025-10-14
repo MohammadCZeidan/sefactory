086933fa87163894e9a09188079d40ce0cb29054
mohammadzeidan <mohammadz20012001@Gmail.com>
2025-10-14 23:04:27 +0300

    i added linked list

--- day10.py
+++ day10.py
@@ -44,3 +44,61 @@ class Queue:
        return self.queue[0]
     def is_empty(self):#check if the queue is empty
         return len(self.queue) == 0
+#linked list implementation of queue
+#CHAINED LIST
+class linked_list:
+    def __init__(self):
+         self.head=None
+    class Node:
+        def __init__(self,data):
+            self.data=data
+            self.next=None
+        def append(self,new_node):#add new node to the end of the linked list
+            current=self.head
+            if current:
+                while current.next:
+                     current=current.next
+                current.next=self.new_node
+            else:
+                    self.head=new_node
+        def display(self):#display the linked list
+            current=self.head
+            while current:
+                print(current.data)
+                current=current.next
+        def delete(self,value):#delete a node from the linked list
+                current=self.head
+                if current:
+                    if current.data==value:
+                        self.head=current.next
+                        current=None
+                        return
+                while current:
+                    if current.data==value:
+                        break
+                    prev=current
+                    current=current.next
+                if current is None:
+                    return
+                prev.next=current.next
+                current=None
+                def insert(self,new_node,position):#insert a new node at a given position
+                    current=self.head
+                    if position==0:
+                        new_node.next=self.head
+                        self.head=new_node
+                        return
+                    count=0
+                    while current:
+                        if count==position:
+                            break
+                        prev=current
+                        current=current.next
+                        count+=1
+                    if current is None:
+                        prev.next=new_node
+                        return
+                    prev.next=new_node
+                    new_node.next=current
+
+    
